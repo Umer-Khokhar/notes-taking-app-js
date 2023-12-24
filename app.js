673a677022a678e9f86129213401be243bf9e526
Umer Khokhar <umerkhokhar1001@gmail.com>
2023-12-24 16:25:37 +0500

    LocalStorage Added

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-const appContainer = document.querySelector('.note-container');
-const createBtn = document.getElementById('btn');
-let notes = document.querySelectorAll('.input-el');
-const inputSection = document.querySelector('.input-section');
+const appContainer = document.querySelector(".note-container");
+const createBtn = document.getElementById("btn");
+let notes = document.querySelectorAll('.input-el')
 
+function saveNotes() {
+  let savedNotes = localStorage.getItem("notes");
 
-createBtn.addEventListener('click', e => {
-  let inputBox = document.createElement('p');
-  inputBox.classList.add('input-el');
-  inputBox.setAttribute('contenteditable', 'true');
-  let image = document.createElement('img');
-  image.src = './images/delete.svg';
-  inputSection.appendChild(inputBox).appendChild(image);
+  if (savedNotes !== null) {
+    appContainer.innerHTML = savedNotes;
+  } else {
+    appContainer.innerHTML = '' // Set to an empty string if no notes are found
+  }
+}
+
+saveNotes();
+
+function updateNotes() {
+  localStorage.setItem("notes", appContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", (e) => {
+  let inputBox = document.createElement("p");
+  inputBox.className = 'input-el';
+  inputBox.setAttribute("contenteditable", "true");
+  let image = document.createElement("img");
+  image.src = "./images/delete.svg";
+  appContainer.appendChild(inputBox).appendChild(image);
+  updateNotes()
+  notes = document.querySelectorAll('.input-el')
 });
 
+appContainer.addEventListener("click", (e) => {
+  const target = e.target;
 
-appContainer.addEventListener('click', e => {
-    if (e.target.tagName === 'IMG') {
-        e.target.parentElement.remove()
-    }
-})
+  if (target.tagName === "IMG") {
+    target.parentElement.remove();
+    updateNotes();
+  } else if (target.tagName === "P") {
+    notes = document.querySelectorAll('.input-el');
+    notes.forEach(nt => {
+      nt.onkeyup = function() {
+        updateNotes()
+      }
+    })
+    };
+});
